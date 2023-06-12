0242c36c933ca1bbaf9fad8e1e92821c80de59eb
Kirill <kiril.bryzgalova@yandex.ru>
2023-06-12 17:30:38 +0200

    initial

---
+++ 4 Модуль.py
@@ -0,0 +1,77 @@
+        # 1 УРОК
+
+# def strcounter(s):  # aab  O(N*M) => O(N*N) => O(N^2)
+#     for sym in set(s):   # ab  M(уник)
+#         counter = 0
+#         for sub_sym in s:   # aab  N(все)
+#             if sym == sub_sym:
+#                 counter += 1
+#         print(sym, counter)
+#
+# strcounter('aabc')
+
+def strcounter(s):  # aab  O(N + M) => O(N + N) => O(2N) => O(N)
+    syms_counter = {}
+    for sym in s:   # aab
+       syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+strcounter('aaaaaaabbbbbbcddddd')
+
+
+        # 2 УРОК
+
+
+
+
+        # 3 УРОК
+
+
+
+
+        # 4 УРОК
+
+
+
+
+        # 5 УРОК
+
+
+
+
+        # 6 УРОК
+
+
+
+
+        # 7 УРОК
+
+
+
+
+        # 8 УРОК
+
+
+
+
+        # 9 УРОК
+
+
+
+
+        # 10 УРОК
+
+
+
+
+        # 11 УРОК
+
+
+
+
+        # 12 УРОК
+
+
+
