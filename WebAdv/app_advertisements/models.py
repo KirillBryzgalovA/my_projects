88c531fdfbbdc08d626a6a0e8bd1f48726f37786
Кирилл <kiril.bryzgalovayandex.ru>
2023-08-02 20:31:14 +0200

    initial

--- WebAdv/app_advertisements/models.py
+++ WebAdv/app_advertisements/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.utils import timezone
 
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length=128)
@@ -9,6 +10,12 @@ class Advertisement(models.Model):
     auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def updated_time(self):
+        today = timezone.now().date()
+        if self.updated_at.date() == today:
+            return f"Сегодня в {self.updated_at.strftime('%H:%M')}"
+        return self.updated_at.strftime('%d.%m.%Y %H:%M')
 
     @admin.display(description='дата создания')
     def created_date(self):
